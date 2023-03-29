2c24539828202117f669ec588aeea4d75c6ecf6b
Nitin <nitanshuchoudhary003@gmail.com>
2023-03-29 19:17:54 +0530

    Image Work

--- TaskOfc/TaskOfc/Controllers/EmployeController.cs
+++ TaskOfc/TaskOfc/Controllers/EmployeController.cs
@@ -31,34 +31,13 @@ namespace TaskOfc.Controllers
         [HttpPost]
         public IActionResult Save([FromBody] Employe employe)
         {
-            if (employe != null)
-            {
-                var file = HttpContext.Request.Form.Files;
-                if (file.Count > 0)
-                {
-                    byte[] pp = null;
-
-                    using (var ps1 = file[0].OpenReadStream())
-                    {
-                        using (var ms1 = new MemoryStream())
-                        {
-                            ps1.CopyTo(ms1);
-                            pp = ms1.ToArray();
-                            var base64 = Convert.ToBase64String(pp);
-                        }
-
-                    }
-
-                }
+            if (employe == null)
+                return NotFound();
+            else
 
                 _context.Employes.Add(employe);
                 _context.SaveChanges();
-            }
-
-            else
-                return BadRequest(ModelState);
-
-            return Ok();
+                 return Ok();
         }
 
             [Route("updateEmploye")]
